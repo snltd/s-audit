dc3cc4a9d076edfc086fcfb15b2d0887c21177fc
snltd <slackboy@gmail.com>
2011-01-11 17:10:50 +0000

    Updated doc.

--- www/docs/extras/index.php
+++ www/docs/extras/index.php
@@ -5,8 +5,8 @@ include("$_SERVER[DOCUMENT_ROOT]/_conf/site_config.php");
 //------------------------------------------------------------------------------
 // SCRIPT STARTS HERE
 
-$menu_entry = "support files";
-$pg = new docPage("support files");
+$menu_entry = "extra files";
+$pg = new docPage("extra files");
 
 ?>
 
@@ -15,7 +15,7 @@ $pg = new docPage("support files");
 <p>Though the <tt>s-audit.sh</tt> tool works with no dependencies, a number
 of additional tools and helpers have grown up around it. Chief among these
 is the PHP interface which presents audit results, but the following tools
-also exist to help you get the most out of s-audit.</p>
+also exist to help you get more information from s-audit.</p>
 
 <dl>
 <dt><a href="smf.php">an SMF manifest</a></dt>
@@ -23,11 +23,18 @@ also exist to help you get the most out of s-audit.</p>
 framework. It is useful to have machines audit themselves on a reboot, or to
 perform on-demand audits simply by refreshing the service.</dd>
 
+<dt><a href="s-audit_pchdefs.php"><tt>s-audit_pchdefs.sh</tt></a></dt>
+<dd>A script which gathers information about Solaris patches, allowing the
+PHP interface to tell you what each installed patch is for.</dd>
+
+<dt><a href="s-audit_pkgdefs.php"><tt>s-audit_pkgdefs.sh</tt></a></dt>
+<dd>A shell script which queries Solaris install images to produce a text
+file which helps PHP interface give more detailed information on installed
+packages.</dd>
+
 </dl>
 
 <?php
 
 $pg->close_page();
 ?>
-
-
