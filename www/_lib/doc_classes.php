9cd5fe88375cb1f930ccc1baaf4c3e1ef81e2cbe
snltd <slackboy@gmail.com>
2011-01-12 17:54:21 +0000

    Adapted to work with new constants, and made a little clearer. Now doesn't print download link if there's no plaintext file in _files/

--- www/_lib/doc_classes.php
+++ www/_lib/doc_classes.php
@@ -16,17 +16,22 @@ class codeBlock {
 
 	// This class prints embedded code blocks
 
-	private $scr;
-	private $scr_src;
-	private $scr_html;
-	private $scr_link;
+	private $scr;		// The name of the code to include (like basename)
+	private $scr_cp;	// path to the raw code
+	private $scr_cl;	// link to raw code
+	private $scr_hp;	// path to HTMLized code
+	private $scr_hl;	// link to HTMLized code
 
 	public function __construct($scr)
 	{
+		// Set variables so show_script() can find everything
+
 		$this->scr = $scr;
-		$this->scr_src = DOC_SCR_DIR . "/$scr";
-		$this->scr_html = $this->scr_src . ".html";
-		$this->scr_link = "/docs/_files/$scr";
+
+		$this->scr_cp = CB_DIR . "/$scr";
+		$this->scr_cl = CB_URL . "/$scr";
+		$this->scr_hp = CB_DIR . "/${scr}.html";
+		$this->scr_hl = CB_URL . "/${scr}.html";
 	}
 
 	public function show_script()
@@ -35,17 +40,20 @@ class codeBlock {
 		// to using the script
 
 		$ret = "\n\n<div class=\"codeBlock\">\n"
-		. "  <div class=\"codeBlockHead\">Source of $this->scr</div>"
-		. "  <div class=\"codeBlockLink\">(<a href=\"$this->scr_link\">"
-		. "view as plain text/download</a>)</div>"
-		. "\n<pre class=\"codeBlock\">";
+		. "  <div class=\"codeBlockHead\">Source of $this->scr</div>";
 
-		if (file_exists($this->scr_html))
-			$ret .= file_get_contents($this->scr_html);
-		elseif (file_exists($this->scr_src))
-			$ret .= file_get_contents($this->scr_src);
+		if (file_exists($this->scr_cp))
+			$ret .= "  <div class=\"codeBlockLink\">(<a href=\""
+			. "$this->scr_cl\">view as plain text/download</a>)</div>";
+
+		$ret .= "\n<pre class=\"codeBlock\">";
+
+		if (file_exists($this->scr_hp))
+			$ret .= file_get_contents($this->scr_hp);
+		elseif (file_exists($this->scr_cp))
+			$ret .= file_get_contents($this->scr_cl);
 		else
-			$ret .= "Someone forgot the code!";
+			$ret .= "Someone forgot the code!<br/>$this->scr_cp";
 
 		return $ret . "\n  </pre>\n  </div>";
 	}
