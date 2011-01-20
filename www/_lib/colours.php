fd51ac3ef3a1dbd448b44653b5342d3dbb011136
snltd <slackboy@gmail.com>
2011-01-20 01:22:45 +0000

    New class names. Separated out nic_cols. Some new colours

--- www/_lib/colours.php
+++ www/_lib/colours.php
@@ -1,24 +1,35 @@
 <?php
 
+//============================================================================
+//
+// colours.php
+// -----------
+//
+// Classes to help colour cells in audit grids and their corresponding keys
+// in documentation. Required by all audit class pages and dynamic_css.php.
+//
+// Part of s-audit. (c) 2011 SearchNet Ltd
+//  see http://snltd.co.uk/s-audit for licensing and documentation
+//
+//============================================================================
+
+// Get the nic_colours[] array. We have to use a _SERVER path because we
+// might be calling this from dynamic_css.php, which doesn't include the
+// site config file
+
+require_once(ROOT . "/_conf/nic_colours.php");
+
+//----------------------------------------------------------------------------
+// colours
+
 class colours {
 
 	// A little class which defines colours used for various purposes, and
 	// provides a method to access their hex values.
 
-	// This array tells us how to colour NICs. The subnets and colours are
-	// site-specific, so you will most likely want to change them.
-
-	static $nic_cols = array(
-		"unconfigured" => "#a8a8a8",// Unconfigured, but plumbed interfaces
-		"vlan" => "#a8a8a8",		// VLANned interfaces
-		"vswitch" => "#7a89bb",		// Virtual Switches
-		"alom" => "#e9a655",		// ALOM cables
-		"192.168.1" => "#c27fac",
-		"10.0.2" => "#62A189",
-		"10.10.8" => "#967e4a",		// brown
-		"10.10.7" => "#61ab52",		// green
-		"10.10.4" => "#f1ef5c",		// yellow
-		"10.10.6" => "#d5554c");	// red
+	static $nic_cols;
+		// This array stores colours for various networks. It's set by the
+		// in the nic_colours.php file
 
 	// These are just named colours we use all over.
 
@@ -31,9 +42,10 @@ class colours {
 		"grey" => "#bbb", 
 		"black" => "#000", 
 		"orange" => "#df8439"
-		);
+	);
 	
-	// $fs_cols is a list of colours used in the filesystem audit page
+	// $fs_cols is a list of colours used in the filesystem audit page.
+	// Keep "smb" and "smbsf" the same
 
 	static $fs_cols = array(
 		"ufs" => "#bfcde0",
@@ -42,11 +54,11 @@ class colours {
 		"hsfs" => "#80d4a0",
 		"lofs" => "#e6e9c9",
 		"vboxfs" => "#b86bae",
-		"smb" => "#7b7a94",		// keep "smb" and "smbsf" the same
+		"smb" => "#7b7a94",		
 		"smbfs" => "#7b7a94",
 		"vdisk" => "#6FBB68",
 		"iscsi" => "#a0b141"
-		);
+	);
 
 	// $ws_cols is a list of colours used to identify different webserver
 	// types
@@ -55,7 +67,7 @@ class colours {
 		"apache" => "#bfcde0",
 		"iPlanet" => "#c7a49c",
 		"nginx" => "#dfdc9b"
-		);
+	);
 	
 	// $db_cols is a list of colours used to identify different databases
 
@@ -63,16 +75,17 @@ class colours {
 		"mysql" => "#BFCDE0",
 		"oracle" => "#c7a49c",
 		"postgres" => "#dfdc9b"
-		);
+	);
 
 	// $stor_cols is a list of colours used to identify different storage
 	// devices
 	
 	static $stor_cols = array(
 		"disk" => "#bfcde0",	// disks
-		"cd" => "#ccc",	// optical drives
+		"cd" => "#ccc",			// optical drives
 		"fc" => "#7b7a94", 		// fibre arrays
-		"tp" => "#c7a49c");		// tape drives
+		"tp" => "#c7a49c"		// tape drives
+	);
 
 	// $plat_cols is a list of colours used to identify different hardware
 	// types
@@ -84,6 +97,8 @@ class colours {
 
 	public function get_col($colour)
 	{
+		// A function used to get the values above.
+
 		if (in_array($colour, array_keys(colours::$cols)))
 			$colour = colours::$cols[$colour];
 
@@ -92,7 +107,10 @@ class colours {
 
 }
 
-class inline_col {
+//----------------------------------------------------------------------------
+// inlineCol
+
+class inlineCol {
 
 	// You can call the following functions with a shorthand name from the
 	// variable list above, or with a hex value. The get_col() function
@@ -111,3 +129,4 @@ class inline_col {
 }
 
 ?>
+
