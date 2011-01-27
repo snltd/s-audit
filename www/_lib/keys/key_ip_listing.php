2a7a5bd00c0aa5833849f65ef25aff6de13feb25
snltd <slackboy@gmail.com>
2011-01-27 12:47:41 +0000

    Don't display key for IP files if they're not present

--- www/_lib/keys/key_ip_listing.php
+++ www/_lib/keys/key_ip_listing.php
@@ -17,15 +17,25 @@ $grid_key = array(
 	// Just one "general" key type that stretches right across the table
 
 	"general" => array(
-		array("live addresses in DNS", "resolved", false),
 		array("addresses taken from audit files", "onlylive", false),
-		array("pingable addresses not in DNS or audit files", "onlyping",
-		false),
-		array("reserved IP addresses", "reserved", false),
+
 		array("pingable on last subnet audit", "boxgreen", false),
 		array("not pingable on last subnet audit", "boxred", false)
 	)
 
 );
+
+// Only add the following if they're relevant
+
+if (file_exists(IP_LIST_FILE)) {
+	$grid_key["general"][] = array("live addresses in DNS", "resolved",
+	false);
+	$grid_key["general"][] = array("pingable addresses not in DNS or audit
+	files", "onlyping", false);
+}
+
+if (file_exists(IP_RES_FILE))
+	$grid_key["general"][] = array("reserved IP addresses", "reserved",
+	false); 
 
 ?>
