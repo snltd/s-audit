9b660cb2b06254473cba5014750e133f020bb3e9
snltd <slackboy@gmail.com>
2011-05-25 00:43:55 +0000

    Fixed broken link

--- www/docs/index.php
+++ www/docs/index.php
@@ -24,21 +24,21 @@ of different kinds of audit. The information it produced soon became
 somewhat overwhelming as plain text, so I wrote a PHP front-end, which has
 grown hand-in-hand with the client script.</p>
 
-<p>s-audit is split into three parts. The <a href="client">client</a> is a
-shell script which is run on the machine you wish to audit. This is the only
-essential, and may prove useful on its own. It is written to be compatible
-with the version of ksh88 shipped with Solaris 2.6, and has no other
-dependencies. Therefore it will run on any Solaris system running today,
-however minimal. The client performs different &quot;classes&quot; of audit,
-looking at different aspects of the machine on which it is run.</p>
+<p>s-audit is split into three parts. The <a href="02_client">client</a> is
+a shell script which is run on the machine you wish to audit. This is the
+only essential, and may prove useful on its own. It is written to be
+compatible with the version of ksh88 shipped with Solaris 2.6, and has no
+other dependencies. Therefore it will run on any Solaris system running
+today, however minimal. The client performs different &quot;classes&quot; of
+audit, looking at different aspects of the machine on which it is run.</p>
 
 <p>The client writes human-readable or machine-parseable information to
 standard out or to files. The files are understood, and turned into pretty,
-coloured grids, by <a href="interface">the interface</a>; a PHP application
-which runs on a web server.</p>
+coloured grids, by <a href="03_interface">the interface</a>; a PHP
+application which runs on a web server.</p>
 
 <p>The information the interface produces can be enhanced and supplemented
-by the output of a number of <a href="support">support</a> programs, some,
+by the output of a number of <a href="04_extras">support programs</a>, some,
 all, or none of which may be useful in any given environment.</p>
 
 <?php
